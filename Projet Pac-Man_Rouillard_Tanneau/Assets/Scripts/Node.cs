eb261edeefbb4eff6d4c4c4ab32941502dc670d4
Chriswann <chriswann@gmail.com>
2020-12-03 10:57:19 +0100

    Node

---
+++ Projet Pac-Man_Rouillard_Tanneau/Assets/Scripts/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int name;
+    public Node[] children;
+    public Vector3 position;
+    
+
+
+
+
+
+
+    public Node(Node[] children, Vector3 position, int name)
+    {
+        this.children = children;
+        this.position = position;
+        this.name = name;
+    }
+    
+}
---
+++ Projet Pac-Man_Rouillard_Tanneau/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    public Queue<Vector3> Pathfind (Node targetposition, Node currentposition)
+    {
+
+        return null;
+    }
+
+}
