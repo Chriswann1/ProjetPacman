e9eafacb1648c748cded2838b17dfdd41d2f9d3c
Chriswann <chriswann@gmail.com>
2020-12-10 08:45:51 +0100

    [wip] EnemyV0

--- Projet Pac-Man_Rouillard_Tanneau/Assets/Scripts/Enemy.cs
+++ Projet Pac-Man_Rouillard_Tanneau/Assets/Scripts/Enemy.cs
@@ -4,14 +4,66 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public enum BehaviourAI
+    {
+        Idle,
+        Attacking,
+        Fleeing
+        
+    }
+    public BehaviourAI ActualBehaviour = BehaviourAI.Idle;
+    
+    
+    private Stack<Vector3> waypoints;
+    
+    public int seed;
+    public int reflex;
+    [SerializeField]private int speed;
+    [SerializeField] private float starttime;
+    
+    
+    private Vector3 position;
+    private GameObject Player;
+    [SerializeField]
+    private Pathfinder _pathfinder;
 
     void Start()
     {
-
+        Player = GameObject.FindWithTag("Player");
+        _pathfinder = GameObject.FindWithTag("GameplayManager").GetComponent<Pathfinder>();
+        StartCoroutine(FindPath());
     }
 
     void Update()
     {
         
     }
+
+    IEnumerator FindPath()
+    {
+        while (true)
+        {
+            switch (ActualBehaviour)
+            {
+                case BehaviourAI.Attacking:
+                    waypoints = _pathfinder.Pathfind(position, Player.transform.position);
+                    yield return new WaitForSeconds(reflex);
+                    break;
+                
+                case BehaviourAI.Fleeing:
+                    
+                    yield return new WaitForSeconds(reflex);
+                    break;
+                
+                case BehaviourAI.Idle:
+                    yield return new WaitForSeconds(starttime);
+                    ActualBehaviour = BehaviourAI.Attacking;
+                    break;
+
+                
+            }
+        }
+    }
+    
+    
 }
